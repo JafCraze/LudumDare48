d2513d6d1331ebfde6480cfff21a979883b2d778
JafCraze <72263490+JafCraze@users.noreply.github.com>
2021-04-26 02:15:16 +0100

    Removed debugging shortcut.

--- Level.gd
+++ Level.gd
@@ -65,9 +65,6 @@ func _unhandled_input(event):
 	if event.is_action_pressed("restart"):
 		get_tree().reload_current_scene()
 
-	if event.is_action_pressed("ui_accept"):
-		$Anim.play("HideHalf")
-	
 	if event.is_action_released("click"):
 		$Laser.hide()
 		if current_ball != null:
